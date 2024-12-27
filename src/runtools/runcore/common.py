f38f57560148172f935cff9ac26026d4a8d87d14
stan <stan@taggstar.com>
2024-12-27 16:49:10 +0700

    super multiple exceptions in phaser hook

--- src/runtools/runcore/common.py
+++ src/runtools/runcore/common.py
@@ -6,3 +6,12 @@ class InvalidStateError(RuntoolsException):
 
     def __init__(self, message: str):
         super().__init__(message)
+
+
+class MultipleExceptions(Exception):
+    """Exception that carries multiple exceptions that occurred during execution"""
+
+    def __init__(self, exceptions: list[Exception]):
+        self.exceptions = exceptions
+        message = f"Multiple exceptions occurred ({len(exceptions)})"
+        super().__init__(message)
