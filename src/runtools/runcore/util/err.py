3f3711d829ea74f137f8cff1ed897cdfb760f70d
stan svec <dev@stansvec.com>
2025-01-18 17:26:52 +0700

    keyboard interrupt handling in env.close

--- src/runtools/runcore/util/err.py
+++ src/runtools/runcore/util/err.py
@@ -1,15 +1,26 @@
-def run_isolated_collect_exceptions(msg, *callbacks) -> None:
+def run_isolated_collect_exceptions(msg, *callbacks, suppress=False) -> None:
     """
     Execute callbacks in order, collecting any exceptions that occur.
     Each callback is executed regardless of whether previous callbacks failed.
+
+    Args:
+        msg: Exception group message if exception is raised
+        *callbacks: Callables to execute in isolation
+        suppress: If True, suppress any exceptions without raising them
     """
     exceptions = []
+    keyboard_interrupted = False
 
     for callback in callbacks:
         try:
             callback()
         except Exception as e:
             exceptions.append(e)
+        except KeyboardInterrupt:
+            keyboard_interrupted = True
 
-    if exceptions:
+    if keyboard_interrupted:
+        raise KeyboardInterrupt
+
+    if exceptions and not suppress:
         raise ExceptionGroup(msg, exceptions)
