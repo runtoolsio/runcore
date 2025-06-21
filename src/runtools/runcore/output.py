064e61c97723fda23d6ed15286a444686b3072cb
stan <stan@taggstar.com>
2025-06-21 10:11:31 +0700

    tail show ordinal

--- src/runtools/runcore/output.py
+++ src/runtools/runcore/output.py
@@ -1,16 +1,17 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum, auto
+from threading import Lock
 from typing import List, Optional
 
 from runtools.runcore import util
 from runtools.runcore.err import InvalidStateError
 
 
-
 class Mode(Enum):
     HEAD = auto()
     TAIL = auto()
+
 
 class Output(ABC):
 
@@ -22,6 +23,7 @@ class Output(ABC):
 @dataclass(frozen=True)
 class OutputLine:
     text: str
+    ordinal: int
     is_error: bool = False
     source: Optional[str] = None
 
@@ -29,6 +31,7 @@ class OutputLine:
     def deserialize(cls, data: dict) -> 'OutputLine':
         return cls(
             text=data["text"],
+            ordinal=data["ordinal"],
             is_error=data["is_error"],
             source=data.get("source"),
         )
@@ -37,9 +40,24 @@ class OutputLine:
         text = util.truncate(self.text, truncate_length, truncated_suffix) if truncate_length is not None else self.text
         return {
             "text": text,
+            "ordinal": self.ordinal,
             "is_error": self.is_error,
             "source": self.source
         }
+
+
+class OutputLineFactory:
+
+    def __init__(self, default_source=None):
+        self.default_source = default_source
+        self._atomic_lock = Lock()
+        self._last = 0
+
+    def __call__(self, text, is_error=False, source=None) -> OutputLine:
+        with self._atomic_lock:
+            self._last = ordinal = self._last + 1
+        return OutputLine(text, ordinal, is_error, source)
+
 
 class OutputObserver(ABC):
 
